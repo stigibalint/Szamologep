5422d81fcdff56944345401e6630ee02ec368c1c
stigibalint <72031994+stigibalint@users.noreply.github.com>
2022-11-04 10:30:30 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,27 @@
+function szamolj(muvelet){
+    var a = parseFloat(document.getElementById('szamA').value);
+    var b = parseFloat(document.getElementById('szamB').value);
+  
+    
+    if (muvelet=="+"){
+        document.getElementById('eredmeny').innerHTML = a+b;
+    }
+    else if(muvelet=="-"){
+        document.getElementById('eredmeny').innerHTML = a-b;
+    }
+    else if(muvelet=="*"){
+        document.getElementById('eredmeny').innerHTML = a*b;
+    }
+    else if(muvelet=="/"){
+        document.getElementById('eredmeny').innerHTML = a/b;
+    }
+}
+
+
+function AddOssze(){
+   szamolj("+");
+}
+function Kivonas(){
+    szamolj("-");
+}
+
